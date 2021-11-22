82c6b3cfd5e74c6b1569f947b727471547251eff
juliatangerino <julianatangerino2017@gmail.com>
2021-11-22 11:11:41 -0300

    Adicionando layout principal e alterando o nome da pasta project

---
+++ projeto/resources/views/welcome.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Trabalho Interdisciplinar</title>
+</head>
+
+<body id="site">
+    <div class="container">
+        <div>
+            <img src="/assets/backgroundassets1.jpg" class="photo" alt="imagem de fundo">
+        </div>
+        <nav>
+            <div class="textDivDireita">
+                <div>
+                    <img src="/assets/Hamburger-menu.png" class="menu-hamburguer" alt="">
+                </div>
+                <ul>
+                    <a href="/home.html">
+                        <li>Home</li>
+                    </a>
+                    <a href="/sobrenos.html">
+                        <li>Sobre nós</li>
+                    </a>
+                    <a href="#">
+                        <li>Serviços</li>
+                    </a>
+                    <a href="#">
+                        <li>Onde estamos</li>
+                    </a>
+                    <a href="#">
+                        <li>Contato</li>
+                    </a>
+                </ul>
+            </div>
+        </nav>
+        <div>
+            <div class="textDivEsquerdaPontinhos">
+                <img src="/assets/pontinhos_vertical.png" alt="pontinhos na vertical">
+            </div>
+            <div class="textDivEsquerdaBaixo">
+                <img src="/assets/raiozinho_vertical.svg" alt="raiozinho na vertical">
+            </div>
+        </div>
+        <div class="triangulos-direita">
+            <img src="/assets/triangulos.svg" alt="triangulos">
+        </div>
+        <div class="direitaBaixo">
+            <img src="/assets/pontinhos_horizontaP.svg" alt="pontinhos na horizontal">
+        </div>
+        <div class="textDivCentro">
+            <img src="/assets/logo.png" class="logo-home" alt="logo">
+            <div>
+                <p class="title-main">Ente querido</p>
+                <p class="subtitle-main">Já possui conta? Faça login!</p>
+            </div>
+        </div>
+        <div class="rodaPe">
+            <p>Arrasta para baixo</p>
+        </div>
+    </div>
+    <div class="containerabout">
+        <div class="ondinhas">
+            <img src="/img_sobrenos/ondinhas.svg" alt="ondinhas">
+        </div>
+        <div class="">
+            <div class="about">
+                <h1>Sobre Nós</h1>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                    classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
+                    professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
+                    literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+                    of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
+                    book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of
+                    Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                    Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in
+                    their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                </p>
+            </div>
+            <div class="photoabout">
+                <img src="/assets/backgroundassets2.jpg" alt="assetsm de fundo">
+            </div>
+        </div>
+        <div class="textDivEsquerdaBaixo">
+            <img src="/img_sobrenos/setinhas.svg" alt="setinhas">
+        </div>
+    </div>
+    <div class="containerServicos">
+        <img src="/assets/backgroundassets.png" alt="Cemitério" class="photoServicos">
+        <img src="/img_servicos/pontao.svg" alt="pontao" class="pontao">
+        <img src="/img_servicos/pontinhos_direita.svg" alt="pontinhos" class="ondinhasServicos">
+        <img src="/img_servicos/piramide.svg" alt="piramide" class="direitaServicos">
+        <img src="/img_servicos/pontinhos_direita_vertical.png" alt="pontinhos detalhes" class="pontinhoDetalhes">
+        <div class="divEsquerda">
+            <h1>CONHEÇA MAIS SOBRE NOSSOS SERVIÇOS.</h1>
+            <p>Selecione uma opção:</p>
+            <div class="btn">
+                <a class="botao-cruz botoes">
+                    <img src="/img_servicos/cruz.svg" alt="cruz" class="cruz">
+                    Sou Cemitério
+                </a>
+            </div>
+            <div class="btn">
+                <a class="botao-tumulo botoes">
+                    <img src="/img_servicos/rip.svg" alt="R.I.P" class="rip">
+                    Quero Realizar Consultas
+                </a>
+            </div>
+        </div>
+        <div class="divDireita">
+            <div class="text-service">
+                <div class="souCemiterio">
+                    <h1> Sou Cemitério</h1>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore molestiae modi dolore
+                        exercitationem dolorum beatae consequuntur officia, reiciendis voluptatum! Aliquam, temporibus
+                        quaerat. Officiis recusandae alias dolorem. Voluptas, rem repellendus.</p>
+                </div>
+                <div class="souCemiterio">
+                    <h1>Quero Realizar Consultas</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas cumque eveniet similique nemo,
+                        voluptatum ea nisi iure provident doloribus. A quasi iste suscipit dolor, molestias quis commodi
+                        facere. Quisquam, architecto? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+                        ratione eaque autem ipsam et ipsa debitis recusandae optio neque, consequuntur earum ipsum sed
+                        voluptas aut atque aspernatur corrupti accusantium nesciunt!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="containerFinal">
+        <img src="/rodapé/backgound.svg" alt="assetsm de fundo" class="photoServicos">
+        <img src="/rodapé/ondinhas_inferior.svg" alt="ondinhas" class="textDivEsquerdaBaixo">
+        <img src="/rodapé/pontos_horizontalSuperior.svg" alt="pontinho na horizontal" class="pontinho_horizontal">
+        <img src="/rodapé/pontos_horizontalSuperior.svg" alt="pontos na horizontal" class="pontinhoInferiorDireito">
+        <img src="/rodapé/raiozinho_vertical.svg" alt="raiozinho img" class="direitaFinal">
+        <div class="notificacao">
+            <h1>Gostaria de saber mais ?</h1>
+            <p>Preencha o formulario abaixo para receber novidades.</p>
+            <div>
+                <div class="form">
+                    <form action="#" method="post" class="form">
+                        <div class="name">
+                            <label for="nome">
+                                <p>Nome:</p>
+                                <input type="text" id="nome" placeholder="Digite seu nome" class="input">
+                            </label>
+                        </div>
+                        <div>
+                            <label for="email">
+                                <p>E-mail</p>
+                                <input type="email" id="email" class="input">
+                            </label>
+                        </div>
+                        <div class="accepted-checkbox">
+                            <input type="checkbox" id="accepted" name="accepted" value="accepted">
+                            <label for="accepted"> Eu gostaria de receber e-mails com novidades </label>
+                        </div>
+                        <div class="submit">
+                            <a href="" type="submit">Enviar</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+        <div class="contact">
+            <a href="#">Privacy</a>
+            <a href="#">Email</a>
+            <a href="#">Terms</a>
+        </div>
+        <div class="social-media">
+            <img src="/assets/facebook.png" alt="Facebook icon">
+            <img src="/assets/twitter.png" alt="Twitter Icon">
+            <img src="/assets/instagram.png" alt="Instagram icon">
+        </div>
+
+    </footer>
+</body>
+
+</html>
+
