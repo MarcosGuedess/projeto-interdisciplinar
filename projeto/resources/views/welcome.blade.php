e9c00a6260d70cbfed29e052875d0d5d1a454cf6
juliatangerino <julianatangerino2017@gmail.com>
2021-12-05 15:56:15 -0300

    Finalizando página home

--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -12,29 +12,19 @@
 <body id="site">
     <div class="container">
         <div>
-            <img src="/assets/backgroundassets1.jpg" class="photo" alt="imagem de fundo">
+            <img src="/assets/backgroundimage1.jpg" class="photo" alt="imagem de fundo">
         </div>
         <nav>
             <div class="textDivDireita">
                 <div>
                     <img src="/assets/Hamburger-menu.png" class="menu-hamburguer" alt="">
                 </div>
-                <ul>
-                    <a href="/home.html">
-                        <li>Home</li>
-                    </a>
-                    <a href="/sobrenos.html">
-                        <li>Sobre nós</li>
-                    </a>
-                    <a href="#">
-                        <li>Serviços</li>
-                    </a>
-                    <a href="#">
-                        <li>Onde estamos</li>
-                    </a>
-                    <a href="#">
-                        <li>Contato</li>
-                    </a>
+                <ul class="menu-items">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre nós</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Onde estamos</a></li>
+                    <li><a href="#">Contato</a></li>    
                 </ul>
             </div>
         </nav>
@@ -56,7 +46,7 @@
             <img src="/assets/logo.png" class="logo-home" alt="logo">
             <div>
                 <p class="title-main">Ente querido</p>
-                <p class="subtitle-main">Já possui conta? Faça login!</p>
+                <a href="#" class="subtitle-main">Já possui conta? Faça login!</a>
             </div>
         </div>
         <div class="rodaPe">
@@ -65,51 +55,52 @@
     </div>
     <div class="containerabout">
         <div class="ondinhas">
-            <img src="/img_sobrenos/ondinhas.svg" alt="ondinhas">
+            <img src="/assets/ondinhas.svg" alt="ondinhas">
         </div>
         <div class="">
             <div class="about">
                 <h1>Sobre Nós</h1>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                    classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
-                    professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
-                    literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-                    of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
-                    book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of
-                    Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
-                    Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in
-                    their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                <p>
+                    Devida a pandemia, doença altamente contagiosa que levou milhares de mortos, houve a necessidade de 
+                    <span>sepultamentos</span> apressados, isso, aliado a recomendação do distanciamento social que não permitiu 
+                    parentes e amigos “velassem” seus entes queridos e em contra partida houve a sobrecarga de 
+                    tarefas dos cemitérios, que tiveram de se adequar aos novos padrões de segurança para evitar o 
+                    contágio de seus colaboradores e a <span>grande demanda de serviços</span> ocasionada pela pandemia.
+                </p>
+                <p>
+                    É <span>fundamental</span> o controle de sepultamentos e a disponibilidade de consultas de 
+                    dados aos entes queridos.
+                    Com a informatização dos serviços pretende-se aprimorar a interação da 
+                    administração com os usuários, em sua maioria visitantes, fornecendo um serviço <span>apropiado
+                    e organizado</span> .
                 </p>
             </div>
             <div class="photoabout">
-                <img src="/assets/backgroundassets2.jpg" alt="assetsm de fundo">
+                <img src="/assets/backgroundimage2.jpg" alt="imagem de fundo">
             </div>
         </div>
         <div class="textDivEsquerdaBaixo">
-            <img src="/img_sobrenos/setinhas.svg" alt="setinhas">
+            <img src="/assets/setinhas.svg" alt="setinhas">
         </div>
     </div>
     <div class="containerServicos">
-        <img src="/assets/backgroundassets.png" alt="Cemitério" class="photoServicos">
-        <img src="/img_servicos/pontao.svg" alt="pontao" class="pontao">
-        <img src="/img_servicos/pontinhos_direita.svg" alt="pontinhos" class="ondinhasServicos">
-        <img src="/img_servicos/piramide.svg" alt="piramide" class="direitaServicos">
-        <img src="/img_servicos/pontinhos_direita_vertical.png" alt="pontinhos detalhes" class="pontinhoDetalhes">
+        <img src="/assets/backgroundimage.png" alt="Cemitério" class="photoServicos">
+        <img src="/assets/pontao.svg" alt="pontao" class="pontao">
+        <img src="/assets/pontinhos_direita.svg" alt="pontinhos" class="ondinhasServicos">
+        <img src="/assets/piramide.svg" alt="piramide" class="direitaServicos">
+        <img src="/assets/pontinhos_direita_vertical.png" alt="pontinhos detalhes" class="pontinhoDetalhes">
         <div class="divEsquerda">
             <h1>CONHEÇA MAIS SOBRE NOSSOS SERVIÇOS.</h1>
             <p>Selecione uma opção:</p>
             <div class="btn">
-                <a class="botao-cruz botoes">
-                    <img src="/img_servicos/cruz.svg" alt="cruz" class="cruz">
+                <a href="#" class="botao-cruz botoes">
+                    <img src="/assets/cruz.svg" alt="cruz" class="cruz">
                     Sou Cemitério
                 </a>
             </div>
             <div class="btn">
-                <a class="botao-tumulo botoes">
-                    <img src="/img_servicos/rip.svg" alt="R.I.P" class="rip">
+                <a href="#" class="botao-tumulo botoes">
+                    <img src="/assets/rip.svg" alt="R.I.P" class="rip">
                     Quero Realizar Consultas
                 </a>
             </div>
@@ -117,52 +108,54 @@
         <div class="divDireita">
             <div class="text-service">
                 <div class="souCemiterio">
-                    <h1> Sou Cemitério</h1>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum labore molestiae modi dolore
-                        exercitationem dolorum beatae consequuntur officia, reiciendis voluptatum! Aliquam, temporibus
-                        quaerat. Officiis recusandae alias dolorem. Voluptas, rem repellendus.</p>
+                    <h1>Para cemitérios</h1>
+                    <p> Acesso ao sistema de gerenciamento de cemitérios, controle de falecimentos, sepultamentos, mapeamento de toda área com informações de tumulos disponiveis e já utilizados.</p>
                 </div>
                 <div class="souCemiterio">
-                    <h1>Quero Realizar Consultas</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas cumque eveniet similique nemo,
-                        voluptatum ea nisi iure provident doloribus. A quasi iste suscipit dolor, molestias quis commodi
-                        facere. Quisquam, architecto? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
-                        ratione eaque autem ipsam et ipsa debitis recusandae optio neque, consequuntur earum ipsum sed
-                        voluptas aut atque aspernatur corrupti accusantium nesciunt!</p>
+                    <h1>Para realizar consultas</h1>
+                    <p>Realizar <span>consulta pública</span> de pessoas sepultadas e a
+                        localização física das respectivas <span>sepulturas</span>.</p>
                 </div>
             </div>
         </div>
     </div>
     <div class="containerFinal">
-        <img src="/rodapé/backgound.svg" alt="assetsm de fundo" class="photoServicos">
-        <img src="/rodapé/ondinhas_inferior.svg" alt="ondinhas" class="textDivEsquerdaBaixo">
-        <img src="/rodapé/pontos_horizontalSuperior.svg" alt="pontinho na horizontal" class="pontinho_horizontal">
-        <img src="/rodapé/pontos_horizontalSuperior.svg" alt="pontos na horizontal" class="pontinhoInferiorDireito">
-        <img src="/rodapé/raiozinho_vertical.svg" alt="raiozinho img" class="direitaFinal">
+        <img src="/assets/backgroundimage2.jpg" alt="imagem de fundo" class="photoform">
+        <img src="/assets/ondinhas_inferior.svg" alt="ondinhas" class="textDivEsquerdaBaixo">
+        <img src="/assets/pontos_horizontalSuperior.svg" alt="pontinho na horizontal" class="pontinho_horizontal">
+        <img src="/assets/pontos_horizontalSuperior.svg" alt="pontos na horizontal" class="pontinhoInferiorDireito">
+        <img src="/assets/raiozinho_vertical.svg" alt="raiozinho img" class="direitaFinal">
         <div class="notificacao">
-            <h1>Gostaria de saber mais ?</h1>
-            <p>Preencha o formulario abaixo para receber novidades.</p>
+            <div class="notificacao-header">
+                <h1>Gostaria de saber mais ?</h1>
+                <p>Preencha o formulario abaixo para receber novidades.</p>
+            </div>
             <div>
                 <div class="form">
                     <form action="#" method="post" class="form">
-                        <div class="name">
-                            <label for="nome">
-                                <p>Nome:</p>
-                                <input type="text" id="nome" placeholder="Digite seu nome" class="input">
-                            </label>
+                        <div class="form-inputs">
+                            <div class="name">
+                                <label for="nome">
+                                    <p>Nome:</p>
+                                    <input type="text" id="nome" placeholder="Digite seu nome" class="input">
+                                </label>
+                            </div>
+                            <div>
+                                <label for="email">
+                                    <p>E-mail</p>
+                                    <input type="email" id="email" class="input" placeholder="Digite seu e-mail">
+                                </label>
+                            </div>
                         </div>
-                        <div>
-                            <label for="email">
-                                <p>E-mail</p>
-                                <input type="email" id="email" class="input">
-                            </label>
-                        </div>
-                        <div class="accepted-checkbox">
-                            <input type="checkbox" id="accepted" name="accepted" value="accepted">
-                            <label for="accepted"> Eu gostaria de receber e-mails com novidades </label>
-                        </div>
-                        <div class="submit">
-                            <a href="" type="submit">Enviar</a>
+                        <div class="shipping-terms">
+                            <div class="accepted-checkbox">
+                                <label for="accepted"> Eu gostaria de receber e-mails com novidades </label>
+                                <input type="checkbox" id="accepted" name="accepted" value="accepted">
+                                
+                            </div>
+                            <div class="submit">
+                                <a href="" type="submit">Enviar</a>
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -171,22 +164,22 @@
     </div>
 
     <footer>
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        <div class="footer-content">
+            <div class="footer-phrase">
+                <p>Você <span>não</span> está sozinho, estamos com você! 💙</p>
+            </div>
+            <div class="contact">
+                <a href="#">Privacy</a>
+                <a href="#">Email</a>
+                <a href="#">Terms</a>
+            </div>
+            <div class="social-media">
+                <img src="/assets/facebook.png" alt="Facebook icon">
+                <img src="/assets/twitter.png" alt="Twitter Icon">
+                <img src="/assets/instagram.png" alt="Instagram icon">
+            </div>
         </div>
-        <div class="contact">
-            <a href="#">Privacy</a>
-            <a href="#">Email</a>
-            <a href="#">Terms</a>
-        </div>
-        <div class="social-media">
-            <img src="/assets/facebook.png" alt="Facebook icon">
-            <img src="/assets/twitter.png" alt="Twitter Icon">
-            <img src="/assets/instagram.png" alt="Instagram icon">
-        </div>
-
     </footer>
 </body>
 
 </html>
-
